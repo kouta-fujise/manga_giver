2fade28b80f945c57b4e7e52530465e0c1609ebc
藤瀬功大 <sketchbooks2490@gmail.com>
2022-04-18 16:34:46 +0900

    add bootstrap update home view(#24)

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,38 +1,40 @@
 
 const payjp = () => {
-  // デバッグ用
-  console.log("js動いている");
-  // console.log(process.env.PAYJP_PUBLIC_KEY);
-  console.log(document.getElementById("number").value);
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const form = document.getElementById("card_form");
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const card = {
-      number: document.getElementById("number").value,
-      name: document.getElementById("name").value,
-      cvc: document.getElementById("cvc").value,
-      exp_month: document.getElementById("exp_month").value,
-      exp_year: `20${document.getElementById("exp_year").value}`,
-    };
-    Payjp.createToken(card, function(status, response) {
-      console.log(status);
-      if (status === 200) {
-        const token = response.id;
-        const tokenObj = `<input value=${token} name='token_id' type="hidden">`;
-        const cardForm = document.getElementById("card_form");
-        cardForm.insertAdjacentHTML("beforeend", tokenObj);
-        document.getElementById("number").removeAttribute("name");
-        document.getElementById("name").removeAttribute("name");
-        document.getElementById("cvc").removeAttribute("name");
-        document.getElementById("exp_month").removeAttribute("name");
-        document.getElementById("exp_year").removeAttribute("name");
-        document.getElementById("card_form").submit();
-      } else {
-        alert("カード情報が正しくありません")
-      }
+  if (document.URL.match(/card/) || document.URL.match(/manga/)){
+    // デバッグ用
+    console.log("js動いている");
+    // console.log(process.env.PAYJP_PUBLIC_KEY);
+    console.log(document.getElementById("number").value);
+    Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+    const form = document.getElementById("card_form");
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const card = {
+        number: document.getElementById("number").value,
+        name: document.getElementById("name").value,
+        cvc: document.getElementById("cvc").value,
+        exp_month: document.getElementById("exp_month").value,
+        exp_year: `20${document.getElementById("exp_year").value}`,
+      };
+      Payjp.createToken(card, function(status, response) {
+        console.log(status);
+        if (status === 200) {
+          const token = response.id;
+          const tokenObj = `<input value=${token} name='token_id' type="hidden">`;
+          const cardForm = document.getElementById("card_form");
+          cardForm.insertAdjacentHTML("beforeend", tokenObj);
+          document.getElementById("number").removeAttribute("name");
+          document.getElementById("name").removeAttribute("name");
+          document.getElementById("cvc").removeAttribute("name");
+          document.getElementById("exp_month").removeAttribute("name");
+          document.getElementById("exp_year").removeAttribute("name");
+          document.getElementById("card_form").submit();
+        } else {
+          alert("カード情報が正しくありません")
+        }
+      });
     });
-  });
+  }
 };
 window.addEventListener("load", payjp);
 
