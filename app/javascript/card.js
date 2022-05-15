9a973a070d94c0e8821fe042e3e5d8e89c56e398
藤瀬功大 <sketchbooks2490@gmail.com>
2022-05-15 22:00:56 +0900

    [try] pay.jp v2(#62)

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,46 @@
+// pay.jp v2対応
+// $(function () {
+//   //URLにcardもしくはmangaが含まれている際に発火します。
+//   if (document.URL.match(/card/) || document.URL.match(/manga/)){
+//
+//     //公開鍵を記述
+//     var payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
+//     //Elements インスタンスを生成します。
+//     var elements = payjp.elements();
+//     var numberElement = elements.create('cardNumber');
+//     var expiryElement = elements.create('cardExpiry');
+//     var cvcElement = elements.create('cardCvc');
+//
+//     numberElement.mount('#payjp_cardNumber');
+//     expiryElement.mount('#expiry-form');
+//     cvcElement.mount('#cvc-form');
+//
+//     var submit_btn = $("#info_submit");
+//     submit_btn.on('click', function (e) {
+//       e.preventDefault();
+//       payjp.createToken(numberElement).then(function (response) {
+//
+//         if (response.error) {  //  通信に失敗したとき
+//           alert(response.error.message)
+//           regist_card.prop('disabled', false)
+//         } else {
+//           $("#card_token").append(
+//             `<input type="hidden" name="payjp_token" value=${response.id}>
+//             <input type="hidden" name="card_token" value=${response.card.id}>`
+//           );
+//           $('#card_form')[0].submit();
+//
+//           $("#card_number").removeAttr("name");
+//           $("#cvc-from").removeAttr("name");
+//           $("#exp_month").removeAttr("name");
+//           $("#exp_year").removeAttr("name");
+//         };
+//       });
+//     });
+//   }
+// });
 
+// pay.jp v1
 const payjp = () => {
   if (document.URL.match(/card/) || document.URL.match(/manga/)){
     // デバッグ用
@@ -38,6 +80,7 @@ const payjp = () => {
 };
 window.addEventListener("load", payjp);
 
+// ゴミ
 // $(function () {
 //   //URLにcardsが含まれている際に発火します。
 //   if (document.URL.match(/card/)){
